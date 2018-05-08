bffddaead04d84a0029aecc1a3aeee8e738306a4
MengChi <mc2311362372@gmail.com>
2018-05-08 15:52:20 +0800

    Revert ".."
    This reverts commit efd247f6ef294d0f071934528a32369b2acf2c33.

---
+++ Assets/TestScripts/LoadABPackage.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadABPackage: MonoBehaviour {
+
+    AssetData newOne;
+    // Use this for initialization
+	void Start () {
+        newOne = new AssetData();
+        newOne.id = 0;
+        newOne.asset = "level01.unity3d";
+        newOne.scene = "level01";
+        AssetBundleManage.Instant.LoadSceneFromFile(newOne);
+    }
+
+}
